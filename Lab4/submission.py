369d06b15926ab2c6a7c952fd4c63f0895c4298a
uohzxela <uohzxela@gmail.com>
2016-09-27 12:50:55 +0800

    refactor lab4 submission

--- Lab4/submission.py
+++ Lab4/submission.py
@@ -3,9 +3,7 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-np.set_printoptions(threshold=np.inf)
-
-def filter(img, x, y, kernel):
+def get_magnitude(img, x, y, kernel):
 	magnitude = 0.
 	for i in xrange(len(kernel)):
 		for j in xrange(len(kernel[0])):
@@ -15,12 +13,12 @@ def filter(img, x, y, kernel):
 	return magnitude
 
 def flip(f):
-	# flip horizontally
+	# flip left and right
 	for i in xrange(len(f)):
 		n = len(f[i])
 		for j in xrange(n/2):
 			f[i][j], f[i][n-j-1] = f[i][n-j-1], f[i][j]
-	# flip vertically
+	# flip up and down
 	for i in xrange(len(f)):
 		n = len(f)
 		for j in xrange(n/2):
@@ -32,7 +30,7 @@ def convolve(img, kernel):
 	result = np.zeros(img.shape)
 	for i in xrange(1, len(result)-1):
 		for j in xrange(1, len(result[0])-1):
-			result[i][j] = filter(img, i, j, kernel)
+			result[i][j] = get_magnitude(img, i, j, kernel)
 	return result
 
 def get_gauss_kernel(size,sigma=1.0):
@@ -47,27 +45,28 @@ def get_gauss_kernel(size,sigma=1.0):
 		kernel = kernel/kernel_sum 
 	return kernel
 
-def compute_responses(W_xx, W_xy, W_yy):
-	responses = []
-	max_response = float('-inf')
-	STEP = 10
-	for i in xrange(STEP, len(W_xx), STEP):
-		for j in xrange(STEP, len(W_xx[0]), STEP):
+#response window
+WINDOW = 10
+
+def compute_response_matrix(W_xx, W_xy, W_yy):
+	response_matrix = np.zeros((W_xx.shape[0]/WINDOW, W_xx.shape[1]/WINDOW))
+	for i in xrange(WINDOW, len(W_xx), WINDOW):
+		for j in xrange(WINDOW, len(W_xx[0]), WINDOW):
 			W = np.array([[W_xx[i][j], W_xy[i][j]], [W_xy[i][j], W_yy[i][j]]])
 			detW = np.linalg.det(W)
 			traceW = np.trace(W)
 			response = detW - (0.06 * traceW * traceW)
-			max_response = max(response, max_response)
-			responses.append((response, i, j))
-	return responses, max_response
+			response_matrix[(i/WINDOW)-1][(j/WINDOW)-1] = response
+	return response_matrix
 
-def find_corners(responses, max_response):
-	# max_response = response_matrix.max()
+def find_corners(response_matrix):
+	max_response = response_matrix.max()
 	corners = []
-	for i in xrange(len(responses)):
-		response, x, y = responses[i]
-		if response >= 0.9 * max_response:
-			corners.append((x, y))
+	for i in xrange(len(response_matrix)):
+		for j in xrange(len(response_matrix[0])):
+			response = response_matrix[i][j]
+			if response >= 0.9 * max_response:
+				corners.append(((i+1)*WINDOW, (j+1)*WINDOW))
 	return corners
 
 SOBEL_X = [[-1,0,1], 
@@ -78,11 +77,14 @@ SOBEL_Y = [[1,2,1],
 		   [0,0,0],
 	       [-1,-2,-1]]
 
+GAUSSIAN = get_gauss_kernel(size=3)
+
 IMG_NAMES = ['checker.jpg', 'flower.jpg', 'test1.jpg', 'test2.jpg', 'test3.jpg']
 
 for name in IMG_NAMES:
 	img = cv2.imread(name, 0)
 	orig = cv2.imread(name)
+
 	gx = convolve(img, SOBEL_X)
 	gy = convolve(img, SOBEL_Y)
 
@@ -90,16 +92,16 @@ for name in IMG_NAMES:
 	I_xy = gx * gy
 	I_yy = gy * gy
 
-	gauss_kernel = get_gauss_kernel(size=3)
-	W_xx = convolve(I_xx, gauss_kernel)
-	W_xy = convolve(I_xy, gauss_kernel)
-	W_yy = convolve(I_yy, gauss_kernel)
+	W_xx = convolve(I_xx, GAUSSIAN)
+	W_xy = convolve(I_xy, GAUSSIAN)
+	W_yy = convolve(I_yy, GAUSSIAN)
 
-	responses, max_response = compute_responses(W_xx, W_xy, W_yy)
-	corners = find_corners(responses, max_response)
+	response_matrix = compute_response_matrix(W_xx, W_xy, W_yy)
+	corners = find_corners(response_matrix)
 	for x, y in corners:
 		cv2.circle(orig, (x, y), 9, (0,255,0), 2)
-	cv2.imwrite(name.split(".")[0] + "_corners.jpg", orig)
+	cv2.imwrite(name.split(".")[0] + "_corners2.jpg", orig)
+
 	# rows, cols = zip(*corners)
 	# plt.figure()
 	# plt.imshow(img, cmap='gray')
